7471abe90324fcf7c8f96e6fc52605d575a4a545
Alek Novkovski <alek@greatcode2.com>
2024-03-11 21:15:58 +0100

    Add CSS styling to dashboard sidebar
    A new CSS module file was created specifically for the dashboard sidebar component. Styling has been applied to elements within the sidebar, improving its visual structure and making it more readable. The sidebar.jsx file was also updated to utilize the new CSS classes.

--- app/ui/dashboard/sidebar/sidebar.jsx
+++ app/ui/dashboard/sidebar/sidebar.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import menuItems from '../../../../constants/sidebar/menuItems'
 import MenuLink from "@/constants/sidebar/menuLink/menuLink";
+import styles from "./sidebar.module.css"
 function Sidebar() {
     return (
-        <div>
-            <ul>
+        <div className={styles.container}>
+            <ul className={styles.list}>
                 {menuItems.map((cat) => (
                     <li key={cat.title}>
-                        <span>{cat.title}</span>
+                        <span className={styles.cat}>{cat.title}</span>
                         {cat.list.map((item) => (
                             <div key={item.title}>
-                                <MenuLink item={item} key={item.title} />
+                                <MenuLink item={item} key={item.title}/>
                             </div>
                         ))}
                     </li>
