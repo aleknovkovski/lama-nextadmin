386c56b3a5df9e0e695e18f84d5db6ba3a69e2b0
Alek Novkovski <alek@greatcode2.com>
2024-03-11 21:23:15 +0100

    Add user profile section to sidebar
    Added new CSS classes in sidebar.module.css and updated the sidebar.jsx file to include a user profile section. This new section is meant to display a user's image, username, and title in the application's sidebar. For now, these are hardcoded image, title and username.

--- app/ui/dashboard/sidebar/sidebar.jsx
+++ app/ui/dashboard/sidebar/sidebar.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import Image from "next/image";
 import menuItems from '../../../../constants/sidebar/menuItems'
 import MenuLink from "@/constants/sidebar/menuLink/menuLink";
 import styles from "./sidebar.module.css"
 function Sidebar() {
     return (
         <div className={styles.container}>
+            <div className={styles.user}>
+                <Image
+                    className={styles.userImage}
+                    src={"/noavatar.png"}
+                    alt=""
+                    width="50"
+                    height="50"
+                />
+                <div className={styles.userDetail}>
+                    <span className={styles.username}>{`{user.username}`}</span>
+                    <span className={styles.userTitle}>Administrator</span>
+                </div>
+            </div>
             <ul className={styles.list}>
                 {menuItems.map((cat) => (
                     <li key={cat.title}>
