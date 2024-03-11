dfa4dee2807502a182769b00e32ae6ae5a925714
Alek Novkovski <alek@greatcode2.com>
2024-03-11 19:40:03 +0100

    Add dynamic menu to sidebar
    Menu items have been populated dynamically in the sidebar component. The menuItems constant is created in a separate file that includes all menu items and corresponding Icons. The react-icons module is also added to the dependencies in package.json.

--- app/ui/dashboard/sidebar/sidebar.jsx
+++ app/ui/dashboard/sidebar/sidebar.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
-
+import menuItems from '../../../../constants/sidebar/menuItems'
 function Sidebar() {
     return (
-        <div>Sidebar</div>
+        <div>
+            <ul>
+                {menuItems.map((cat) => (
+                    <li key={cat.title}>
+                        <span>{cat.title}</span>
+                        {cat.list.map((item) => (
+                            <div key={item.title}>
+                                <span>{item.icon}</span>
+                                <span>{item.title}</span>
+                            </div>
+                        ))}
+                    </li>
+                ))}
+            </ul>
+        </div>
     )
 }
 
