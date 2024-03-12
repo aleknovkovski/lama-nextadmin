902b7c5f18e18f09543e63722c1937a687827025
Alek Novkovski <alek@greatcode2.com>
2024-03-12 18:21:52 +0100

    Create UsersPage overall structure, layout and styling
     The code introduces a layout for browsing, searching, and managing users, along with adding related CSS styles to enhance the page's appearance. The page includes a search bar, a button for adding a new user, a table layout for displaying user information, and action buttons such as 'View' and 'Delete'. Below the table there is a pagination component, currently represented as a component. User data is currently represented by text placeholders, and search input represented by a regular unstyled, static input.

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import styles from "@/app/ui/dashboard/users/users.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
-function UsersPage() {
-    return (
-        <div>UsersPage</div>
-    )
-}
+const UsersPage = async () => {
 
-export default UsersPage
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <input placeholder="Search for a user..."></input>
+        <Link href="/dashboard/users/add">
+          <button className={styles.addButton}>Add New</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Created At</td>
+            <td>Role</td>
+            <td>Status</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image
+                    src={"/noavatar.png"}
+                    alt=""
+                    width={40}
+                    height={40}
+                    className={styles.userImage}
+                  />
+                {`{user.username}`}
+              </div>
+            </td>
+              <td>{`{user.email}`}</td>
+              <td>{`{user.createdAt`}</td>
+              <td>{`{user.isAdmin}`}</td>
+              <td>{`{user.isActive}`}</td>
+              <td>
+                <div className={styles.buttons}>
+                  <Link href={`/dashboard/users/userid`}> {/*make userid dynamic*/}
+                    <button className={`${styles.button} ${styles.view}`}>
+                      View
+                    </button>
+                  </Link>
+                    <input type="hidden" name="id" value={`(user.id)`} />
+                    <button className={`${styles.button} ${styles.delete}`}>
+                      Delete
+                    </button>
+                </div>
+              </td>
+          </tr>
+        </tbody>
+      </table>
+        {/*<Pagination/>*/}
+    </div>
+  );
+};
+
+export default UsersPage;
