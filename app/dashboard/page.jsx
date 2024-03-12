5af40f6e51fd2d081d6caeee59c3b387168789b3
Alek Novkovski <alek@greatcode2.com>
2024-03-12 01:00:26 +0100

    Create basic dashboard layout structure
    Added CSS classes for the dashboard layout and implemented them in the dashboard page component. This includes a wrapper around the page, a container for main content including cards and charts, and a side panel. Placeholder texts were also added in the jsx file to help visualize the structure and flow of elements.

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import styles from "@/app/ui/dashboard/dashboard.module.css"
 
 function Dashboard() {
     return (
-        <div>Dashboard</div>
-    )
+        <div className={styles.wrapper}>
+            <div className={styles.main}>
+                <div className={styles.cards}>
+                    Card
+                    Card
+                    Card
+                </div>
+                Transactions
+                Chart
+            </div>
+            <div className={styles.side}>
+                Rightbar
+            </div>
+        </div>
+    );
 }
 
 export default Dashboard
