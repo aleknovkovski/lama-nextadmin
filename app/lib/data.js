7d423e9c198299b7438d2814cb568c1ae1f324a1
Alek Novkovski <alek@greatcode2.com>
2024-03-15 15:13:05 +0100

    Implement page param support in fetchUsers and UsersPage
    The code changes include UsersPage grabbing the page number from searchParams and providing it to fetchUsers and there using it to limit and offset the number of results returned by the fetchUsers function. In addition, the search component resets the page number any time a user starts searching.

--- app/lib/data.js
+++ app/lib/data.js
@@ -1,12 +1,16 @@
 import { User } from "./models";
 import { connectToDatabase } from "../lib/mongoose"
 
-export const fetchUsers = async (q) => {
+export const fetchUsers = async (q, page) => {
   const regex = new RegExp(q, "i");
+
+  const ITEM_PER_PAGE = 2;
 
   try {
     connectToDatabase();
     const users = await User.find({ username: { $regex: regex } })
+      .limit(ITEM_PER_PAGE)
+      .skip(ITEM_PER_PAGE * (page - 1));
     return { users };
   } catch (err) {
     console.log(err);
