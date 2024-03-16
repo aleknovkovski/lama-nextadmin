223cd9d85a496853faa029e9eb136ac4b4839f1d
Alek Novkovski <alek@greatcode2.com>
2024-03-16 22:24:06 +0100

    Update sidebar to display dynamic user info
    The sidebar has been updated to display user information dynamically based on the session data loaded from the auth module. It now shows the authenticated user's image and role. Additionally, the auth module has been enhanced to include more user details in the session.

--- app/ui/dashboard/sidebar/sidebar.jsx
+++ app/ui/dashboard/sidebar/sidebar.jsx
@@ -4,20 +4,23 @@ import menuItems from '../../../../constants/sidebar/menuItems'
 import MenuLink from "@/app/ui/dashboard/sidebar/menuLink/menuLink";
 import styles from "./sidebar.module.css"
 import {MdLogout} from "react-icons/md";
-function Sidebar() {
+import {auth} from "@/app/auth";
+async function Sidebar() {
+    const { user } = await auth();
+    console.log({user})
     return (
         <div className={styles.container}>
             <div className={styles.user}>
                 <Image
                     className={styles.userImage}
-                    src={"/noavatar.png"}
+                    src={user.img || "/noavatar.png"}
                     alt=""
                     width="50"
                     height="50"
                 />
                 <div className={styles.userDetail}>
-                    <span className={styles.username}>{`{user.username}`}</span>
-                    <span className={styles.userTitle}>Administrator</span>
+                    <span className={styles.username}>{user.username}</span>
+                    <span className={styles.userTitle}>{user.isAdmin ? "Administrator" : "User"}</span>
                 </div>
             </div>
             <ul className={styles.list}>
