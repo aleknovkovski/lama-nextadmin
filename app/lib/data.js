6682c8c90c09b51768bc73f6ffd566751d9eaca0
Alek Novkovski <alek@greatcode2.com>
2024-03-16 10:56:35 +0100

    Implement product search and pagination
    The ProductPage component now uses an asynchronous function to fetch products based on search parameters and renders them accordingly. The fetchProducts function has been added to interact with the database and retrieve products based on page number and search query, implementing pagination for product listing. The frontend has been updated to render results dynamically.

--- app/lib/data.js
+++ app/lib/data.js
@@ -1,4 +1,4 @@
-import { User } from "./models";
+import {Product, User} from "./models";
 import { connectToDatabase } from "../lib/mongoose"
 
 export const fetchUsers = async (q, page) => {
@@ -16,5 +16,24 @@ export const fetchUsers = async (q, page) => {
   } catch (err) {
     console.log(err);
     throw new Error("Failed to fetch users!");
+  }
+};
+
+export const fetchProducts = async (q, page) => {
+  console.log(q);
+  const regex = new RegExp(q, "i");
+
+  const ITEM_PER_PAGE = 2;
+
+  try {
+    connectToDatabase();
+    const count = await Product.find({ title: { $regex: regex } }).count();
+    const products = await Product.find({ title: { $regex: regex } })
+      .limit(ITEM_PER_PAGE)
+      .skip(ITEM_PER_PAGE * (page - 1));
+    return { count, products };
+  } catch (err) {
+    console.log(err);
+    throw new Error("Failed to fetch products!");
   }
 };
